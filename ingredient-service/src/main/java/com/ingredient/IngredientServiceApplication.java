72319c167e300f2d3787d11c8b0e6be79dd2f3ae
anoop-rana <anooprana45@gmail.com>
2020-08-23 15:50:01 +0530

    recipe, user, ingredient and eureka micro services created. close #1, #2, #3, #4

---
+++ ingredient-service/src/main/java/com/ingredient/IngredientServiceApplication.java
@@ -0,0 +1,15 @@
+package com.ingredient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class IngredientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngredientServiceApplication.class, args);
+	}
+
+}
